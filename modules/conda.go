945c3f9cc549336dcba9a94b8def33eb6d5a66bd
Eliseo Martelli <me@eliseomartelli.it>
2024-03-11 08:59:33 +0100

    feat(conda): start developing module

---
+++ modules/conda.go
@@ -0,0 +1,29 @@
+package modules
+
+import "github.com/spf13/cobra"
+
+const condaModuleName = "conda"
+
+func init() { Register(condaModuleName, func() Module { return &condaModule{} }) }
+
+type condaModule struct{}
+
+// BulkRun implements Module.
+func (c *condaModule) BulkRun(config *Config) error {
+	return nil
+}
+
+// CliConfig implements Module.
+func (c *condaModule) CliConfig(config *Config) *cobra.Command {
+	return nil
+}
+
+// Commit implements Module.
+func (c *condaModule) Commit(config *Config, result any) error {
+	return nil
+}
+
+// Run implements Module.
+func (c *condaModule) Run(any) error {
+	return nil
+}
