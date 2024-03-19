abe57e9839f279b8a8e6738f4180c483e9536172
Eliseo Martelli <me@eliseomartelli.it>
2024-03-19 10:21:36 +0100

    feat: add cran initial implementation

--- modules/cran.go
+++ modules/cran.go
@@ -1,9 +1,15 @@
 package modules
 
 import (
+	"credo/logger"
+	"credo/project"
 	"fmt"
+	"os"
+	"path"
 	"strings"
 
+	rcran "github.com/CREDOProject/go-rcran"
+	rscript "github.com/CREDOProject/go-rscript"
 	"github.com/spf13/cobra"
 )
 
@@ -12,6 +18,8 @@ const cranModuleName = "cran"
 const cranModuleShort = "Retrieves a CRAN package and its dependencies."
 
 const cranModuleExample = `
+Install a package from CRAN
+	credo cran abind
 `
 
 // Registers the carnModule.
@@ -21,9 +29,31 @@ func init() { Register(cranModuleName, func() Module { return &cranModule{} }) }
 type cranModule struct{}
 
 type cranSpell struct {
-	PackageName      string `yaml:"package_name,omitempty"`
-	PackageDirectory string `yaml:"package_directory,omitempty"`
-	Repository       string `yaml:"repository,omitempty"`
+	PackageName string `yaml:"package_name,omitempty"`
+	PackagePath string `yaml:"package_path,omitempty"`
+	Repository  string `yaml:"repository,omitempty"`
+}
+
+// spellFromInstallOptions returns a new cran spell from *rcran.InstallOptions.
+func (*cranModule) spellFromInstallOptions(
+	options *rcran.InstallOptions,
+) *cranSpell {
+	spell := &cranSpell{
+		PackageName: options.PackageName,
+		Repository:  options.Repository,
+	}
+	return spell
+}
+
+// spellFromDownloadOptions returns a new cran spell from *rcran.DownloadOptions.
+func (*cranModule) spellFromDownloadOptions(
+	options *rcran.DownloadOptions,
+) *cranSpell {
+	spell := &cranSpell{
+		PackageName: options.PackageName,
+		Repository:  options.Repository,
+	}
+	return spell
 }
 
 // equals checks if two cranSpell objects are equal.
@@ -47,11 +77,48 @@ func (c *cranModule) BulkRun(config *Config) error {
 	return nil
 }
 
+func (m *cranModule) bareRun(c cranSpell) (*cranSpell, error) {
+	bin, err := rscript.DetectRscriptBinary()
+	if err != nil {
+		return nil, err
+	}
+	tempdir := os.TempDir()
+	downloadOptions := &rcran.DownloadOptions{
+		PackageName:          c.PackageName,
+		DestinationDirectory: tempdir,
+		Repository:           c.Repository,
+	}
+	cmd, err := rcran.Download(downloadOptions)
+	if err != nil {
+		return nil, err
+	}
+	script, err := rscript.New(bin).Evaluate(cmd).Seal()
+	if err != nil {
+		return nil, err
+	}
+	out, err := script.CombinedOutput()
+	if err != nil {
+		return nil, err
+	}
+	logger.Get().Print(string(out))
+	finalSpell := m.spellFromDownloadOptions(downloadOptions)
+	path, err := rcran.ParsePath(string(out))
+	if err != nil {
+		return nil, err
+	}
+	finalSpell.PackagePath = path
+	return finalSpell, nil
+}
+
 // cobraArgs is used to validate the arguments passed to the cran command.
 //
 // This function is intended to be used by cobra.
 func (c *cranModule) cobraArgs() func(*cobra.Command, []string) error {
-	return func(c *cobra.Command, s []string) error {
+	return func(c *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return fmt.Errorf("%s module requires at least one argument.",
+				cranModuleName)
+		}
 		return nil
 	}
 }
@@ -60,37 +127,81 @@ func (c *cranModule) cobraArgs() func(*cobra.Command, []string) error {
 // It serves as an entry point to the cranModule.
 //
 // This function is inteded to be used by cobra.
-func (c *cranModule) cobraRun(_ *Config) func(*cobra.Command, []string) {
-	return func(ccmd *cobra.Command, s []string) {
+func (c *cranModule) cobraRun(cfg *Config) func(*cobra.Command, []string) {
+	return func(cmd *cobra.Command, args []string) {
+		packageName := args[0]
+		repository, _ := cmd.Flags().GetString("repository")
+		spell, err := c.bareRun(cranSpell{
+			PackageName: packageName,
+			Repository:  repository,
+		})
+		if err != nil {
+			logger.Get().Fatal(err)
+		}
+		err = c.Commit(cfg, spell)
+		if err != nil {
+			logger.Get().Fatal(err)
+		}
 	}
 }
 
 // CliConfig implements Module.
 func (c *cranModule) CliConfig(config *Config) *cobra.Command {
-	return &cobra.Command{
+	command := &cobra.Command{
 		Args:    c.cobraArgs(),
 		Example: pipModuleExample,
 		Run:     c.cobraRun(config),
 		Short:   cranModuleShort,
 		Use:     cranModuleName,
 	}
+	command.PersistentFlags().String("repository", "", "Repository to use.")
+	return command
 }
 
 // Commit implements Module.
 func (c *cranModule) Commit(config *Config, result any) error {
-	newEntry, ok := result.(cranSpell)
+	newEntry, ok := result.(*cranSpell)
 	if !ok {
 		return fmt.Errorf("Error Converting") //TODO: unify errors.
 	}
-	if Contains(config.Cran, newEntry) {
+	if Contains(config.Cran, *newEntry) {
 		return ErrAlreadyPresent
 	}
-	config.Cran = append(config.Cran, newEntry)
+	config.Cran = append(config.Cran, *newEntry)
 	return nil
 }
 
 // Run implements Module.
-func (c *cranModule) Run(any) error {
-	// TODO: implement Run
-	return nil
+func (c *cranModule) Run(anyspell any) error {
+	spell, ok := anyspell.(cranSpell)
+	if !ok {
+		return fmt.Errorf("Error converting")
+	}
+	project, err := project.ProjectPath()
+	if err != nil {
+		return err
+	}
+	bin, err := rscript.DetectRscriptBinary()
+	if err != nil {
+		return err
+	}
+	destinationDirectory := path.Join(*project, cranModuleName)
+	err = os.MkdirAll(destinationDirectory, 0755)
+	if err != nil {
+		return err
+	}
+	downloadOptions := &rcran.DownloadOptions{
+		PackageName:          spell.PackageName,
+		DestinationDirectory: destinationDirectory,
+		Repository:           spell.Repository,
+	}
+	cmd, err := rcran.Download(downloadOptions)
+	if err != nil {
+		return err
+	}
+	script, err := rscript.New(bin).Evaluate(cmd).Seal()
+	script.Stdout = os.Stdout
+	script.Stderr = os.Stderr
+	err = script.Run()
+	return err
 }
