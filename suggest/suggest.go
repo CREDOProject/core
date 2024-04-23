dfd3300a7b29d7e866f7f529832c480f0e70dcb8
Eliseo Martelli <me@eliseomartelli.it>
2024-04-23 11:57:50 +0200

    docs: documentation of suggest

--- suggest/suggest.go
+++ suggest/suggest.go
@@ -2,22 +2,31 @@ package suggest
 
 import "fmt"
 
-var suggestions []Suggestion = []Suggestion{}
+// Represents the internal list of suggestions.
+var suggestions Suggestions = []Suggestion{}
 
+// Suggestion represents a suggestion made from a credo Module.
 type Suggestion struct {
-	Module    string
-	From      string
+	// Module is the name of the module that the suggestion came from.
+	Module string
+	// From is the origin source of the suggestion, a.k.a. the package that
+	// originated it.
+	From string
+	// Suggested is the suggested package.
 	Suggested string
 }
 
+// Register adds a new suggestion to the internal list of suggestions.
 func Register(suggest Suggestion) {
 	suggestions = append(suggestions, suggest)
 }
 
+// Get returns the internal list of all registered suggestions.
 func Get() Suggestions {
 	return suggestions
 }
 
+// Returns a formatted string representation of a suggestion.
 func (s Suggestion) String() string {
 	return fmt.Sprintf("%s suggested from %s coming from %s.",
 		s.Suggested,
@@ -25,8 +34,10 @@ func (s Suggestion) String() string {
 		s.Module)
 }
 
+// Suggestions represents a list of Suggetion
 type Suggestions []Suggestion
 
+// Returns a formatted string representation of the suggestions.
 func (suggestions Suggestions) String() (output string) {
 	for _, s := range suggestions {
 		output += fmt.Sprintf("\t- %s\n", s.String())
@@ -34,4 +45,5 @@ func (suggestions Suggestions) String() (output string) {
 	return
 }
 
+// HasSuggestion checks if there are any registered suggestions.
 func HasSuggestion() bool { return len(suggestions) > 0 }
