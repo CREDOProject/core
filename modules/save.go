d211f4c5ae0e11123d9d057f4c162da301294197
Eliseo Martelli <me@eliseomartelli.it>
2024-04-26 10:45:37 +0200

    chore: rearrange save module

--- modules/save.go
+++ modules/save.go
@@ -1,6 +1,8 @@
 package modules
 
 import (
+	"credo/logger"
+
 	"github.com/spf13/cobra"
 )
 
@@ -8,6 +10,24 @@ const saveModuleName = "save"
 
 // Registers the applyModule.
 func init() { Register(saveModuleName, func() Module { return &saveModule{} }) }
+
+// CliConfig implements Module.
+func (m *saveModule) CliConfig(conifig *Config) *cobra.Command {
+	return &cobra.Command{
+		Use:   saveModuleName,
+		Short: "Runs the credospell.yaml configuration in the current directory and saves every dependency.",
+		Run: func(cmd *cobra.Command, args []string) {
+			for k := range Modules {
+				module := Modules[k]()
+				err := module.BulkSave(conifig)
+				if err != nil {
+					logger.Get().Fatal(err)
+				}
+			}
+		},
+		Args: cobra.NoArgs,
+	}
+}
 
 // saveModule is used to apply the credospell configuration in the current
 // working directory.
@@ -18,30 +38,6 @@ func (m *saveModule) Apply(any) error { return nil }
 
 // This is a stub method. It should always return nil.
 func (m *saveModule) BulkApply(config *Config) error { return nil }
-
-func (m *saveModule) bulkRun(c *Config) error {
-	// Iterates ovet the modules to call the bulkRun function of each Module.
-	for k := range Modules {
-		module := Modules[k]() // Returns the module.
-		err := module.BulkSave(c)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-// CliConfig implements Module.
-func (m *saveModule) CliConfig(conifig *Config) *cobra.Command {
-	return &cobra.Command{
-		Use:   saveModuleName,
-		Short: "Runs the credospell.yaml configuration in the current directory and saves every dependency.",
-		Run: func(cmd *cobra.Command, args []string) {
-			m.bulkRun(conifig)
-		},
-		Args: cobra.NoArgs,
-	}
-}
 
 // This is a stub method. It should always return nil.
 func (m *saveModule) Commit(config *Config, result any) error { return nil }
