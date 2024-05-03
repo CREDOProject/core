cecd43ffa01ba21242b930f6885d132d94c783d9
Eliseo Martelli <me@eliseomartelli.it>
2024-05-03 10:44:43 +0200

    feat: rework cran

--- modules/cran.go
+++ modules/cran.go
@@ -9,10 +9,10 @@ import (
 	"path"
 	"strings"
 
-	rcran "github.com/CREDOProject/go-rcran"
-	rdepends "github.com/CREDOProject/go-rdepends"
-	"github.com/CREDOProject/go-rdepends/providers"
-	rscript "github.com/CREDOProject/go-rscript"
+	gorcran "github.com/CREDOProject/go-rcran"
+	gordepends "github.com/CREDOProject/go-rdepends"
+	gordependsP "github.com/CREDOProject/go-rdepends/providers"
+	gorscript "github.com/CREDOProject/go-rscript"
 	"github.com/CREDOProject/sharedutils/filter"
 	"github.com/spf13/cobra"
 )
@@ -30,14 +30,22 @@ Install a package from BioConductor
 	credo bioconductor GenomicRanges
 `
 
-// Registers the carnModule.
-func init() { Register(cranModuleName, func() Module { return &cranModule{} }) }
+type cranSpell struct {
+	PackageName          string      `yaml:"package_name,omitempty"`
+	PackagePath          string      `yaml:"package_path,omitempty"`
+	Repository           string      `yaml:"repository,omitempty"`
+	BioConductor         bool        `yaml:"bioconductor,omitempty"`
+	Dependencies         []cranSpell `yaml:"dependencies,omitempty"`
+	ExternalDependencies Config      `yaml:"external_dependencies,omitempty"`
+}
 
-// cranModule is used to manage the CARN scope in the credospell configuration.
-type cranModule struct{}
+// Registers the cranModule.
+func init() { Register(cranModuleName, func() Module { return &cranModule2{} }) }
+
+type cranModule2 struct{}
 
 // Apply implements Module.
-func (c *cranModule) Apply(anyspell any) error {
+func (c *cranModule2) Apply(anyspell any) error {
 	spell, ok := anyspell.(cranSpell)
 	if !ok {
 		return ErrConverting
@@ -52,34 +60,28 @@ func (c *cranModule) Apply(anyspell any) error {
 			return err
 		}
 	}
-	project, err := project.ProjectPath()
+	destdir, err := c.destinationDirectory()
 	if err != nil {
 		return err
 	}
-	bin, err := rscript.DetectRscriptBinary()
+	libraryDir, err := c.libraryDirectory()
 	if err != nil {
 		return err
 	}
-	destinationDirectory := path.Join(*project, cranModuleName)
-	err = os.MkdirAll(destinationDirectory, 0755)
-	if err != nil {
-		return err
-	}
-	libraryDirectory := path.Join(*project, "R-library")
-	err = os.MkdirAll(libraryDirectory, 0755)
-	if err != nil {
-		return err
-	}
-	installOptions := &rcran.InstallOptions{
-		PackageName: path.Join(destinationDirectory, spell.PackagePath),
+	localInstallOptions := gorcran.InstallOptions{
+		PackageName: path.Join(destdir, spell.PackagePath),
 		Repository:  "NULL",
-		Lib:         libraryDirectory,
+		Lib:         libraryDir,
 	}
-	cmd, err := rcran.InstallLocal(installOptions)
+	cmd, err := gorcran.InstallLocal(&localInstallOptions)
 	if err != nil {
 		return err
 	}
-	script, err := rscript.New(bin).Evaluate(cmd).Seal()
+	bin, err := gorscript.DetectRscriptBinary()
+	if err != nil {
+		return err
+	}
+	script, err := gorscript.New(bin).Evaluate(cmd).Seal()
 	script.Stdout = os.Stdout
 	script.Stderr = os.Stderr
 	err = script.Run()
@@ -87,7 +89,7 @@ func (c *cranModule) Apply(anyspell any) error {
 }
 
 // BulkApply implements Module.
-func (c *cranModule) BulkApply(config *Config) error {
+func (c *cranModule2) BulkApply(config *Config) error {
 	for _, cs := range config.Cran {
 		if err := c.Apply(cs); err != nil {
 			return err
@@ -96,48 +98,8 @@ func (c *cranModule) BulkApply(config *Config) error {
 	return nil
 }
 
-type cranSpell struct {
-	PackageName          string      `yaml:"package_name,omitempty"`
-	PackagePath          string      `yaml:"package_path,omitempty"`
-	Repository           string      `yaml:"repository,omitempty"`
-	BioConductor         bool        `yaml:"bioconductor,omitempty"`
-	Dependencies         []cranSpell `yaml:"dependencies,omitempty"`
-	ExternalDependencies Config      `yaml:"external_dependencies,omitempty"`
-}
-
-// spellFromDownloadOptions returns a new cran spell from *rcran.DownloadOptions.
-func (*cranModule) spellFromDownloadOptions(
-	options *rcran.DownloadOptions,
-) *cranSpell {
-	spell := &cranSpell{
-		PackageName: options.PackageName,
-		Repository:  options.Repository,
-	}
-	return spell
-}
-
-// equals checks if two cranSpell objects are equal.
-func (c cranSpell) equals(t equatable) bool {
-	// TODO: implement equality check.
-	s, ok := t.(cranSpell)
-	if !ok {
-		return false
-	}
-	equality := len(s.Dependencies) == len(c.Dependencies)
-	if !equality {
-		return false
-	}
-	for i := range s.Dependencies {
-		equality = equality &&
-			s.Dependencies[i].equals(c.Dependencies[i])
-	}
-	return equality && strings.Compare(s.PackageName, c.PackageName) == 0 &&
-		strings.Compare(s.PackagePath, c.PackagePath) == 0 &&
-		s.BioConductor == c.BioConductor
-}
-
 // BulkSave implements Module.
-func (c *cranModule) BulkSave(config *Config) error {
+func (c *cranModule2) BulkSave(config *Config) error {
 	for _, cs := range config.Cran {
 		if err := c.Save(cs); err != nil {
 			return err
@@ -146,160 +108,105 @@ func (c *cranModule) BulkSave(config *Config) error {
 	return nil
 }
 
-func (m *cranModule) bareRun(c cranSpell, cfg *Config) (*cranSpell, error) {
-	if c.BioConductor {
-		err := m.installBioconductor(cfg)
-		if err != nil {
-			return nil, err
-		}
+// CliConfig implements Module.
+func (c *cranModule2) CliConfig(config *Config) *cobra.Command {
+	command := &cobra.Command{
+		Args:    c.cobraArgs(),
+		Example: cranModuleExample,
+		Run:     c.cobraRun(config),
+		Short:   cranModuleShort,
+		Use:     cranModuleName,
+		Aliases: []string{bioconductorModuleName},
 	}
-	bin, err := rscript.DetectRscriptBinary()
-	if err != nil {
-		return nil, err
-	}
-	finalSpell, err := m.bareRunSingle(c, bin, c.BioConductor)
-	// Retrieve dependencies
-	cmd := ""
-	if c.BioConductor {
-		cmd, err = rcran.GetBioconductorDepenencies(&rcran.InstallOptions{
-			PackageName: c.PackageName,
-			Repository:  c.Repository,
-			DryRun:      true,
-		})
-	} else {
-		cmd, err = rcran.GetDependencies(&rcran.InstallOptions{
-			PackageName: c.PackageName,
-			Repository:  c.Repository,
-			DryRun:      true,
-		})
-	}
-	if err != nil {
-		return nil, err
-	}
-	script, err := rscript.New(bin).Evaluate(cmd).Seal()
-	if err != nil {
-		return nil, err
-	}
-	out, err := script.Output()
-	outString := string(out)
-	if err != nil {
-		return nil, err
-	}
-	outClean := strings.Trim(outString, "\n")
-	dependenciesNames := strings.Split(outClean, "\n")
-	if len(dependenciesNames) > 0 {
-		for _, dep := range dependenciesNames {
-			if dep == "" {
-				continue
-			}
-			depSpell, err := m.bareRunSingle(cranSpell{
-				PackageName:  dep,
-				Repository:   c.Repository,
-				BioConductor: false,
-			}, bin, c.BioConductor)
-			if err != nil {
-				continue
-			}
-			if !Contains(finalSpell.Dependencies, *depSpell) {
-				finalSpell.Dependencies = append(finalSpell.Dependencies,
-					*depSpell)
-			}
-		}
-	}
-	return finalSpell, nil
+	command.PersistentFlags().String("repository", "", "Repository to use.")
+	return command
 }
 
-func (m *cranModule) bareRunSingle(
-	c cranSpell,
-	bin string,
-	bioconductor bool,
-) (*cranSpell, error) {
-	tempdir := os.TempDir()
-	downloadOptions := &rcran.DownloadOptions{
-		PackageName:          c.PackageName,
-		DestinationDirectory: tempdir,
-		Repository:           c.Repository,
+// Commit implements Module.
+func (c *cranModule2) Commit(config *Config, result any) error {
+	newEntry, ok := result.(*cranSpell)
+	if !ok {
+		return ErrConverting
 	}
-	var cmd string
-	var err error
-	if bioconductor {
-		cmd, err = rcran.DownloadBioconductor(downloadOptions)
-	} else {
-		cmd, err = rcran.Download(downloadOptions)
+	if newEntry == nil {
+		return nil
 	}
-	if err != nil {
-		return nil, err
+	if Contains(config.Cran, *newEntry) {
+		return ErrAlreadyPresent
 	}
-	script, err := rscript.New(bin).Evaluate(cmd).Seal()
-	if err != nil {
-		return nil, err
+	config.Cran = append(config.Cran, *newEntry)
+	return nil
+}
+
+// Save implements Module.
+func (c *cranModule2) Save(anyspell any) error {
+	spell, ok := anyspell.(cranSpell)
+	if !ok {
+		return ErrConverting
 	}
-	out, err := script.CombinedOutput()
-	outputString := string(out)
-	logger.Get().Print(outputString)
-	if err != nil {
-		return nil, err
-	}
-	finalSpell := m.spellFromDownloadOptions(downloadOptions)
-	finalSpell.BioConductor = bioconductor
-	path, err := rcran.ParsePath(outputString)
-	if err != nil {
-		return nil, err
-	}
-	finalSpell.PackagePath = path
-	pkgPath, err := rcran.GetPath(outputString)
-	if err != nil {
-		return nil, err
-	}
-	additionalDependencies, err := rdepends.DependsOn(pkgPath)
-	if err != nil {
-		return nil, err
-	}
-	suggestions := filter.Filter(additionalDependencies, _onlySuggestions)
-	for _, s := range suggestions {
-		suggest.Register(suggest.Suggestion{
-			Module:    cranModuleName,
-			From:      finalSpell.PackageName,
-			Suggested: s.Name,
-		})
-	}
-	for _, d := range additionalDependencies {
-		module, ok := Modules[d.PackageManager]
-		if ok {
-			args := []string{d.Name}
-			module().CliConfig(&finalSpell.ExternalDependencies).Run(nil, args)
+	for _, dep := range spell.Dependencies {
+		if err := c.Save(dep); err != nil {
+			return err
 		}
 	}
-	return finalSpell, nil
-}
-
-func _onlySuggestions(d providers.Dependency) bool {
-	return d.Suggestion
-}
-
-// installBioconductor runs the command in an opinionated fashion to install
-// bioconductor.
-//
-// Used if cranSpell.BioConductor is set.
-func (m *cranModule) installBioconductor(cfg *Config) error {
-	spell, err := m.bareRun(cranSpell{
-		PackageName:  "BiocManager",
-		BioConductor: false,
-	}, cfg)
+	destdir, err := c.destinationDirectory()
 	if err != nil {
 		return err
 	}
-	err = m.Commit(cfg, spell)
-	if err == ErrAlreadyPresent {
+	err = DeepSave(&spell.ExternalDependencies)
+	if err != nil {
 		return nil
 	}
+	downloadFunction := c.downloadFunction(spell.BioConductor)
+	cmd, err := downloadFunction(&gorcran.DownloadOptions{
+		PackageName:          spell.PackageName,
+		DestinationDirectory: destdir,
+		Repository:           spell.Repository,
+	})
+	if err != nil {
+		return err
+	}
+	bin, err := gorscript.DetectRscriptBinary()
+	if err != nil {
+		return err
+	}
+	script, err := gorscript.New(bin).Evaluate(cmd).Seal()
+	script.Stdout = os.Stdout
+	script.Stderr = os.Stderr
+	err = script.Run()
 	return err
+}
+
+func (c *cranModule2) destinationDirectory() (string, error) {
+	project, err := project.ProjectPath()
+	if err != nil {
+		return "", err
+	}
+	directory := path.Join(*project, cranModuleName)
+	err = os.MkdirAll(directory, 0755)
+	if err != nil {
+		return "", err
+	}
+	return directory, nil
+}
+
+func (c *cranModule2) libraryDirectory() (string, error) {
+	project, err := project.ProjectPath()
+	if err != nil {
+		return "", err
+	}
+	directory := path.Join(*project, "R-Library")
+	err = os.MkdirAll(directory, 0755)
+	if err != nil {
+		return "", err
+	}
+	return directory, nil
 }
 
 // cobraArgs is used to validate the arguments passed to the cran command.
 //
 // This function is intended to be used by cobra.
-func (c *cranModule) cobraArgs() func(*cobra.Command, []string) error {
+func (c *cranModule2) cobraArgs() func(*cobra.Command, []string) error {
 	return func(c *cobra.Command, args []string) error {
 		if len(args) < 1 {
 			return fmt.Errorf("%s module requires at least one argument.",
@@ -313,7 +220,7 @@ func (c *cranModule) cobraArgs() func(*cobra.Command, []string) error {
 // It serves as an entry point to the cranModule.
 //
 // This function is inteded to be used by cobra.
-func (c *cranModule) cobraRun(cfg *Config) func(*cobra.Command, []string) {
+func (c *cranModule2) cobraRun(cfg *Config) func(*cobra.Command, []string) {
 	return func(cmd *cobra.Command, args []string) {
 		isBioconductor := strings.Compare(
 			cmd.CalledAs(),
@@ -336,82 +243,169 @@ func (c *cranModule) cobraRun(cfg *Config) func(*cobra.Command, []string) {
 	}
 }
 
-// CliConfig implements Module.
-func (c *cranModule) CliConfig(config *Config) *cobra.Command {
-	command := &cobra.Command{
-		Args:    c.cobraArgs(),
-		Example: cranModuleExample,
-		Run:     c.cobraRun(config),
-		Short:   cranModuleShort,
-		Use:     cranModuleName,
-		Aliases: []string{bioconductorModuleName},
-	}
-	command.PersistentFlags().String("repository", "", "Repository to use.")
-	return command
-}
-
-// Commit implements Module.
-func (c *cranModule) Commit(config *Config, result any) error {
-	newEntry, ok := result.(*cranSpell)
-	if !ok {
-		return ErrConverting
-	}
-	if newEntry == nil {
-		return nil
-	}
-	if Contains(config.Cran, *newEntry) {
-		return ErrAlreadyPresent
-	}
-	config.Cran = append(config.Cran, *newEntry)
-	return nil
-}
-
-// Save implements Module.
-func (c *cranModule) Save(anyspell any) error {
-	spell, ok := anyspell.(cranSpell)
-	if !ok {
-		return ErrConverting
-	}
-	for _, dep := range spell.Dependencies {
-		err := c.Save(dep)
+func (c *cranModule2) bareRun(s cranSpell, cfg *Config) (*cranSpell, error) {
+	if s.BioConductor {
+		err := c.installBioConductor(cfg)
 		if err != nil {
-			return err
+			return nil, err
 		}
 	}
-	project, err := project.ProjectPath()
+	return c.bareRunSingle(s)
+}
+
+func (c *cranModule2) bareRunSingle(s cranSpell) (*cranSpell, error) {
+	rscriptBin, err := gorscript.DetectRscriptBinary()
+	if err != nil {
+		return nil, err
+	}
+	tempdir := os.TempDir()
+	downloadOptions := &gorcran.DownloadOptions{
+		PackageName:          s.PackageName,
+		DestinationDirectory: tempdir,
+		Repository:           s.Repository,
+	}
+	downloadFunction := c.downloadFunction(s.BioConductor)
+	cmd, err := downloadFunction(downloadOptions)
+	if err != nil {
+		return nil, err
+	}
+	script, err := gorscript.New(rscriptBin).Evaluate(cmd).Seal()
+	if err != nil {
+		return nil, err
+	}
+	out, err := script.CombinedOutput()
+	outputString := string(out)
+	logger.Get().Print(outputString)
+	if err != nil {
+		return nil, err
+	}
+	pkgPath, err := gorcran.GetPath(outputString)
+	if err != nil {
+		return nil, err
+	}
+	deps, err := c.getDependencies(rscriptBin, s)
+	if err != nil {
+		return nil, err
+	}
+	additionalDependencies, err := gordepends.DependsOn(pkgPath)
+	if err != nil {
+		return nil, err
+	}
+	// Register suggestions.
+	suggestions := filter.Filter(additionalDependencies,
+		func(a gordependsP.Dependency) bool { return a.Suggestion })
+	for _, suggestion := range suggestions {
+		suggest.Register(suggest.Suggestion{
+			Module:    cranModuleName,
+			From:      s.PackageName,
+			Suggested: suggestion.Name,
+		})
+	}
+	finalSpell := cranSpell{
+		PackageName:  s.PackageName,
+		PackagePath:  pkgPath,
+		Repository:   s.Repository,
+		BioConductor: s.BioConductor,
+		Dependencies: deps,
+	}
+	for _, d := range additionalDependencies {
+		module, ok := Modules[d.PackageManager]
+		if ok {
+			args := []string{d.Name}
+			module().CliConfig(&finalSpell.ExternalDependencies).Run(nil, args)
+		}
+	}
+	return &finalSpell, nil
+}
+
+func (c *cranModule2) getDependencies(rscriptBin string, s cranSpell) ([]cranSpell, error) {
+	dependencyFunction := c.dependencyFunction(s.BioConductor)
+	cmd, err := dependencyFunction(&gorcran.InstallOptions{
+		PackageName: s.PackageName,
+		Repository:  s.Repository,
+		DryRun:      false,
+	})
+	if err != nil {
+		return nil, err
+	}
+	script, err := gorscript.New(rscriptBin).Evaluate(cmd).Seal()
+	if err != nil {
+		return nil, err
+	}
+	out, err := script.Output()
+	outputString := string(out)
+	logger.Get().Print(outputString)
+	if err != nil {
+		return nil, err
+	}
+	dependencyList := strings.Split(strings.Trim(outputString, "\n"), "\n")
+	deps := []cranSpell{}
+	for _, dep := range dependencyList {
+		if dep == "" {
+			continue
+		}
+		dependencySpell, err := c.bareRunSingle(cranSpell{
+			PackageName:  dep,
+			Repository:   s.Repository,
+			BioConductor: s.BioConductor,
+		})
+		if !Contains(deps, *dependencySpell) {
+			deps = append(deps, *dependencySpell)
+		}
+		if err != nil {
+			continue
+		}
+	}
+	return deps, nil
+}
+
+func (c *cranModule2) dependencyFunction(bioconductor bool) func(
+	o *gorcran.InstallOptions) (string, error) {
+	if bioconductor {
+		return gorcran.GetBioconductorDepenencies
+	}
+	return gorcran.GetDependencies
+}
+
+func (c *cranModule2) downloadFunction(bioconductor bool) func(
+	o *gorcran.DownloadOptions) (string, error) {
+	if bioconductor {
+		return gorcran.DownloadBioconductor
+	}
+	return gorcran.Download
+}
+
+func (c *cranModule2) installBioConductor(cfg *Config) error {
+	spell, err := c.bareRun(cranSpell{
+		PackageName:  "BiocManager",
+		BioConductor: false,
+	}, cfg)
 	if err != nil {
 		return err
 	}
-	bin, err := rscript.DetectRscriptBinary()
-	if err != nil {
-		return err
-	}
-	destinationDirectory := path.Join(*project, cranModuleName)
-	err = os.MkdirAll(destinationDirectory, 0755)
-	if err != nil {
-		return err
-	}
-	downloadOptions := &rcran.DownloadOptions{
-		PackageName:          spell.PackageName,
-		DestinationDirectory: destinationDirectory,
-		Repository:           spell.Repository,
-	}
-	var cmd string
-	if spell.BioConductor {
-		cmd, err = rcran.DownloadBioconductor(downloadOptions)
-	} else {
-		cmd, err = rcran.Download(downloadOptions)
-	}
-	if err != nil {
-		return err
-	}
-	err = DeepSave(&spell.ExternalDependencies)
-	if err != nil {
+	err = c.Commit(cfg, spell)
+	if err == ErrAlreadyPresent {
 		return nil
 	}
-	script, err := rscript.New(bin).Evaluate(cmd).Seal()
-	script.Stdout = os.Stdout
-	script.Stderr = os.Stderr
-	err = script.Run()
 	return err
+}
+
+// equals checks if two cranSpell objects are equal.
+func (c cranSpell) equals(t equatable) bool {
+	// TODO: implement equality check.
+	s, ok := t.(cranSpell)
+	if !ok {
+		return false
+	}
+	equality := len(s.Dependencies) == len(c.Dependencies)
+	if !equality {
+		return false
+	}
+	for i := range s.Dependencies {
+		equality = equality &&
+			s.Dependencies[i].equals(c.Dependencies[i])
+	}
+	return equality && strings.Compare(s.PackageName, c.PackageName) == 0 &&
+		strings.Compare(s.PackagePath, c.PackagePath) == 0 &&
+		s.BioConductor == c.BioConductor
 }
