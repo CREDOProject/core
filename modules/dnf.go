06fa2ca62482c519301388deea0e718f0140d785
Eliseo Martelli <me@eliseomartelli.it>
2024-09-30 17:03:57 +0200

    feat(modules/dnf): initial scaffolding of cliconfig

--- modules/dnf.go
+++ modules/dnf.go
@@ -1,6 +1,11 @@
 package modules
 
-import "github.com/spf13/cobra"
+import (
+	"credo/logger"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
 
 const dnfModuleName = "dnf"
 
@@ -33,7 +38,42 @@ func (d *dnfModule) BulkSave(config *Config) error {
 
 // CliConfig implements Module.
 func (d *dnfModule) CliConfig(config *Config) *cobra.Command {
-	panic("unimplemented")
+	return &cobra.Command{
+		Args:    d.cobraArgs(),
+		Example: dnfModuleExample,
+		Run:     d.cobraRun(config),
+		Short:   dnfModuleShort,
+		Use:     dnfModuleName,
+	}
+}
+
+// Function used to run the module from the command line.
+// It serves as an entry point to the bare run of the dnfModule.
+//
+// Intended to be used by cobra.
+func (d *dnfModule) cobraRun(config *Config) func(*cobra.Command, []string) {
+	return func(cmd *cobra.Command, args []string) {
+		// TODO: implement run
+		err := d.Commit(config, nil)
+		if err != nil && err != ErrAlreadyPresent {
+			logger.Get().Fatal(err)
+		}
+	}
+}
+
+// Function used to validate the arguments passed to the dnf command.
+// If no arguments are passed, it returns an error.
+// Otherwise it returns nil.
+//
+// Intended to be used by cobra.
+func (*dnfModule) cobraArgs() func(*cobra.Command, []string) error {
+	return func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return fmt.Errorf("%s module requires at least one argument.",
+				aptModuleName)
+		}
+		return nil
+	}
 }
 
 // Commit implements Module.
